6f5ca0562b98092ac71965ce485f92b2ec348be7
ethan townsend <eetfitness@gmail.com>
2019-10-17 21:47:14 -0700

    update

--- server/app.js
+++ server/app.js
@@ -21,8 +21,24 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({'extended': true}));
 app.use(morgan('dev'));
 
-app.use(express.static(path.join(__dirname, '../dist/nodequiz')));
-app.use('/', express.static(path.join(__dirname, '../dist/nodequiz')));
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PATCH, PUT, DELETE, OPTIONS"
+  );
+  next();
+});
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// app.use(express.static(path.join(__dirname, '../dist/nodequiz')));
+// app.use('/', express.static(path.join(__dirname, '../dist/nodequiz')));
 
 app.use('/api', api);
 
